4ff8e885a8846af63bf578f4ce964ef741bc947b
Chewsday <z647623@gmail.com>
2025-05-19 15:33:33 +0800

    fix some little thing-2

--- backend/server.ts
+++ backend/server.ts
@@ -41,7 +41,7 @@ if (ENV_VARS.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '/frontend/dist')))
 
 
-  app.get(/\*/, (req, res) => {
+  app.get(/\/*/, (req, res) => {
     res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html'))
   })
 }
